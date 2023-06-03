ce7714adbba4e4a6480382bf9c1da4cbd043b723
Olufisayo Israel Tomide <115098511+KodarkBlack@users.noreply.github.com>
2023-06-03 03:14:27 +0100

    commit

--- checkout.js
+++ checkout.js
@@ -1,57 +1,14 @@
-// let afterjustsomeminutes = document.querySelector("afterjustsomeminutes")
-// let makeenoconfuseme = document.querySelectorAll("makeenoconfuseme")
-// let currentIndex = 0
-// let intervalTime = 5000
 
-// function startCarousel() {
-//     makeenoconfuseme[currentIndex].classList.add("active")
-//     console.log(makeenoconfuseme)
-// }
+let carouselItems = document.querySelectorAll('.carousel-items');
+let numbersOfItems = 5
 
-// Get the carousel container
-// let carouselContainer = document.querySelector(".carousel-container");
-
-// Get the carousel items
-// let carouselItems = document.querySelectorAll(".carousel-item");
-
-// Set the initial index
-// let currentIndex = 0;
-
-// Set the interval time (in milliseconds)
-// let intervalTime = 5000;
-
-// Set the interval ID variable
-// let intervalID;
-
-
-// Function to start the carousel
-// function startCarousel() {
-  // Set the initial active item
-  // carouselItems[currentIndex].classList.add("active");
-
-  // Set the interval to change the active item
-  // intervalID = setInterval(function() {
-    // Remove the active class from the current item
-    // carouselItems[currentIndex].classList.remove("active");
-
-    // Increment the index
-    // currentIndex++;
-
-    // Check if the index is greater than or equal to the number of items
-    // if (currentIndex >= carouselItems.length) {
-    //   currentIndex = 0;
-    // }
-
-    // Set the next item to active
-    // carouselItems[currentIndex].classList.add("next");
-
-    // Wait for the transition to complete, then remove the next class and set the new active item
-//     setTimeout(function() {
-//       carouselItems[currentIndex].classList.remove("next");
-//       carouselItems[currentIndex].classList.add("active");
-//     }, 500);
-//   }, intervalTime);
-// }
-
-// Call the startCarousel function to begin the carousel
-// startCarousel();
+function startCarousel() {
+  for (let numbersOfItems = 0; numbersOfItems.length; numbersOfItems++){
+    const carouselItems = document.querySelectorAll('.carousel-items');
+    const translateXValue = -numbersOfItems * 100;
+    carouselItems.forEach(item => {
+      item.style.transform = `translateX(${translateXValue}%)`;
+  });
+  }
+}
+startCarousel()
